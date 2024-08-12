79bc0b900d7bf9c1a2fdb018ba513b463a686db2
Goswami2021Vaibhav <goswamivaibhav72@gmail.com>
2024-08-12 21:57:59 +0530

    Creating database connection

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,16 +1,18 @@
+'use client'
 import {
     Sheet,
     SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
     SheetTrigger,
 } from "@/components/ui/sheet"
-import { SignedIn, UserButton } from "@clerk/nextjs"
+import { navLinks } from "@/constants"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { Button } from "../ui/button"
 
 const MobileNav = () => {
+    const pathname = usePathname()
     return (
         <header className="header">
             <Link href='/' className="flex items-center gap-2 md:py-2">
@@ -19,23 +21,51 @@ const MobileNav = () => {
             <nav className="flex gap-2">
                 <SignedIn>
                     <UserButton />
+                    <Sheet>
+                        <SheetTrigger>
+                            <Image src="/assets/icons/menu.svg" alt="menu" width={32} height={32} className="cursor-pointer" />
+                        </SheetTrigger>
+                        <SheetContent className="sheet-content sm:w-64">
+                            <>
+                                <Image src="/assets/images/logo-text.svg" alt="logo" width={152} height={23} />
+
+                                <ul className="header-nav_elements">
+                                    {navLinks.map((link) => {
+                                        const isActive = link.route === pathname
+
+                                        return (
+                                            <li key={link.route}
+                                                className={`${isActive && 'gradient-text'}  p-18 flex whitespace-nowrap text-dark-700`}
+                                            >
+                                                <Link className="sidebar-link cursor-pointer" href={link.route}>
+                                                    <Image
+                                                        src={link.icon}
+                                                        alt="logo"
+                                                        width={24}
+                                                        height={24}
+
+                                                    />
+                                                    {link.label}
+                                                </Link>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </>
+                        </SheetContent>
+                    </Sheet>
                 </SignedIn>
 
-                <Sheet>
-                    <SheetTrigger>Open</SheetTrigger>
-                    <SheetContent>
-                        <SheetHeader>
-                            <SheetTitle>Are you absolutely sure?</SheetTitle>
-                            <SheetDescription>
-                                This action cannot be undone. This will permanently delete your account
-                                and remove your data from our servers.
-                            </SheetDescription>
-                        </SheetHeader>
-                    </SheetContent>
-                </Sheet>
+                <SignedOut>
+                    <Button asChild className="button bg-purple-gradient bg-cover">
+                        <Link href="/sign-in">Login</Link>
+                    </Button>
+                </SignedOut>
+
             </nav>
         </header>
     )
 }
 
 export default MobileNav
+
