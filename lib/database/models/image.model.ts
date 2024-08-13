bb0b149a3534a38d8147200d48e8350ca0ba425e
Vaibhav <goswamivaibhav72@gmail.com>
2024-08-14 00:58:38 +0530

    updating collection component

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,8 +1,8 @@
-import { Schema, models, model, Document } from "mongoose";
-
+import { Document, Schema, model, models } from "mongoose";
 
 export interface IImage extends Document {
     title: string;
+    transformationType: string;
     publicId: string;
     secureURL: string;
     width?: number;
@@ -16,9 +16,10 @@ export interface IImage extends Document {
         _id: string;
         firstName: string;
         lastName: string;
-    },
+    }
+    createdAt?: Date;
+    updatedAt?: Date;
 }
-
 
 const ImageSchema = new Schema({
     title: { type: String, required: true },
@@ -33,10 +34,10 @@ const ImageSchema = new Schema({
     color: { type: String },
     prompt: { type: String },
     author: { type: Schema.Types.ObjectId, ref: 'User' },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
 
-}, { timestamps: true });
+const Image = models?.Image || model('Image', ImageSchema);
 
-
-const Image = models?.Image || model('Image', ImageSchema)
-
-export default Image
+export default Image;
